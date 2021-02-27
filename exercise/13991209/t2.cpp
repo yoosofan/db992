b2a46897ae003a4a818e7ab9f4f8c440c77688ba
Ahmad Yoosofan <yoosofan@gmx.com>
2021-02-27 10:44:10 +0330

    %s

--- exercise/13991209/t2.cpp
+++ exercise/13991209/t2.cpp
@@ -48,7 +48,7 @@ void Add_goods ()
   scanf ("%d", &p_t[counter_p].code_of_product);
 
   printf ("enter Name Of Product\n");
-  scanf ("%s", &p_t[counter_p].product_name);
+  scanf ("%s", p_t[counter_p].product_name);
 
   printf ("enter Popularity\n");
   scanf ("%d", &p_t[counter_p].popularity);
@@ -66,7 +66,7 @@ void Add_goods ()
   scanf ("%d", &p_t[counter_p].available_number);
 
   printf ("enter Compony_name  \n");
-  scanf ("%s", &p_t[counter_p].producer_compony);
+  scanf ("%s", p_t[counter_p].producer_compony);
 
   printf ("enter Date of produce\n");
   scanf ("%d", &p_t[counter_p].produce_date);
@@ -81,7 +81,7 @@ void Add_goods ()
 void Add_customer ()
 {
   printf ("enter Name\n");
-  scanf ("%s", &c_t[counter_c].name);
+  scanf ("%s", c_t[counter_c].name);
 
   printf ("enter Id \n");
   scanf ("%d", &c_t[counter_c].id);
@@ -89,11 +89,11 @@ void Add_customer ()
   c_t[counter_c].Number_of_times_purchased = 0;
 
   printf ("enter address of customer\n");
-  scanf ("%s", &c_t[counter_c].address);
+  scanf ("%s", c_t[counter_c].address);
 
 
   printf ("enter phone_number\n");
-  scanf ("%s", &c_t[counter_c].phone_number);
+  scanf ("%s", c_t[counter_c].phone_number);
 
   counter_c++;
   menu ();
@@ -124,15 +124,15 @@ void show_list_goods ()
   printf ("hello");
   for (int i = 0; i <= counter_p; i++)
     {
-      printf ("%d",p_t[i].code_of_product);
+      printf ("%d", &p_t[i].code_of_product);
       printf ("%s",p_t[i].product_name);
-      printf ("%d",p_t[i].popularity);
-      printf ("%d",p_t[i].ext);
-      printf ("%d",p_t[i].produce_date);
-      printf ("%d",p_t[i].price_one);
-      printf ("%d",p_t[i].whole_sale);
-      printf ("%d",p_t[i].remaining);
-      printf ("%d",p_t[i].available_number);
+      printf ("%d", &p_t[i].popularity);
+      printf ("%d", &p_t[i].ext);
+      printf ("%d", &p_t[i].produce_date);
+      printf ("%d", &p_t[i].price_one);
+      printf ("%d", &p_t[i].whole_sale);
+      printf ("%d", &p_t[i].remaining);
+      printf ("%d", &p_t[i].available_number);
       printf (p_t[i].producer_compony);
       printf("______________________");
     }
@@ -266,3 +266,110 @@ main ()
   menu ();
 
 }
+/*
+13991209$ g++ t2.cpp 
+t2.cpp: In function ‘void Add_goods()’:
+t2.cpp:51:12: warning: format ‘%s’ expects argument of type ‘char*’, but argument 2 has type ‘char (*)[50]’ [-Wformat=]
+   51 |   scanf ("%s", &p_t[counter_p].product_name);
+      |           ~^   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      |            |   |
+      |            |   char (*)[50]
+      |            char*
+t2.cpp:60:12: warning: format ‘%f’ expects argument of type ‘float*’, but argument 2 has type ‘int*’ [-Wformat=]
+   60 |   scanf ("%f", &p_t[counter_p].price_one);
+      |           ~^   ~~~~~~~~~~~~~~~~~~~~~~~~~
+      |            |   |
+      |            |   int*
+      |            float*
+      |           %d
+t2.cpp:63:12: warning: format ‘%f’ expects argument of type ‘float*’, but argument 2 has type ‘int*’ [-Wformat=]
+   63 |   scanf ("%f", &p_t[counter_p].whole_sale);
+      |           ~^   ~~~~~~~~~~~~~~~~~~~~~~~~~~
+      |            |   |
+      |            |   int*
+      |            float*
+      |           %d
+t2.cpp:77:3: error: ‘menu’ was not declared in this scope
+   77 |   menu ();
+      |   ^~~~
+t2.cpp: In function ‘void Add_customer()’:
+t2.cpp:84:12: warning: format ‘%s’ expects argument of type ‘char*’, but argument 2 has type ‘char (*)[50]’ [-Wformat=]
+   84 |   scanf ("%s", &c_t[counter_c].name);
+      |           ~^   ~~~~~~~~~~~~~~~~~~~~
+      |            |   |
+      |            |   char (*)[50]
+      |            char*
+t2.cpp:92:12: warning: format ‘%s’ expects argument of type ‘char*’, but argument 2 has type ‘char (*)[200]’ [-Wformat=]
+   92 |   scanf ("%s", &c_t[counter_c].address);
+      |           ~^   ~~~~~~~~~~~~~~~~~~~~~~~
+      |            |   |
+      |            |   char (*)[200]
+      |            char*
+t2.cpp:96:12: warning: format ‘%s’ expects argument of type ‘char*’, but argument 2 has type ‘char (*)[20]’ [-Wformat=]
+   96 |   scanf ("%s", &c_t[counter_c].phone_number);
+      |           ~^   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      |            |   |
+      |            |   char (*)[20]
+      |            char*
+t2.cpp:99:3: error: ‘menu’ was not declared in this scope
+   99 |   menu ();
+      |   ^~~~
+t2.cpp: In function ‘void sele_process()’:
+t2.cpp:108:12: warning: format ‘%d’ expects argument of type ‘int*’, but argument 2 has type ‘long int*’ [-Wformat=]
+  108 |   scanf ("%d", &s_t[counter_s].code_of_product);
+      |           ~^   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      |            |   |
+      |            |   long int*
+      |            int*
+      |           %ld
+t2.cpp:119:3: error: ‘menu’ was not declared in this scope
+  119 |   menu();
+      |   ^~~~
+t2.cpp: In function ‘void show_list_goods()’:
+t2.cpp:136:22: error: invalid conversion from ‘char’ to ‘const char*’ [-fpermissive]
+  136 |       printf (p_t[i].producer_compony);
+      |               ~~~~~~~^~~~~~~~~~~~~~~~
+      |                      |
+      |                      char
+In file included from /usr/include/c++/9/cstdio:42,
+                 from t2.cpp:1:
+/usr/include/stdio.h:332:43: note:   initializing argument 1 of ‘int printf(const char*, ...)’
+  332 | extern int printf (const char *__restrict __format, ...);
+      |                    ~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~
+t2.cpp:136:38: warning: format not a string literal and no format arguments [-Wformat-security]
+  136 |       printf (p_t[i].producer_compony);
+      |                                      ^
+t2.cpp: In function ‘void show_list_customer()’:
+t2.cpp:153:5: error: ‘menu’ was not declared in this scope
+  153 |     menu();
+      |     ^~~~
+t2.cpp: In function ‘void show_list_sales()’:
+t2.cpp:162:17: warning: format ‘%d’ expects argument of type ‘int’, but argument 2 has type ‘long int’ [-Wformat=]
+  162 |       printf ("%d\n ",s_t[i].code_of_product);
+      |                ~^     ~~~~~~~~~~~~~~~~~~~~~~
+      |                 |            |
+      |                 int          long int
+      |                %ld
+t2.cpp:169:5: error: ‘menu’ was not declared in this scope
+  169 |     menu();
+      |     ^~~~
+t2.cpp:183:23: warning: format ‘%d’ expects argument of type ‘int’, but argument 2 has type ‘long int’ [-Wformat=]
+  183 |             printf ("%d\n ",s_t[i].code_of_product);
+      |                      ~^     ~~~~~~~~~~~~~~~~~~~~~~
+      |                       |            |
+      |                       int          long int
+      |                      %ld
+t2.cpp:191:5: error: ‘menu’ was not declared in this scope
+  191 |     menu();
+      |     ^~~~
+t2.cpp: In function ‘void show_list_sales_today()’:
+t2.cpp:205:23: warning: format ‘%d’ expects argument of type ‘int’, but argument 2 has type ‘long int’ [-Wformat=]
+  205 |             printf ("%d\n ",s_t[i].code_of_product);
+      |                      ~^     ~~~~~~~~~~~~~~~~~~~~~~
+      |                       |            |
+      |                       int          long int
+      |                      %ld
+t2.cpp:213:5: error: ‘menu’ was not declared in this scope
+  213 |     menu();
+      |     
+*/
