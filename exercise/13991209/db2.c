3925e6cc77f187faa3f40e2b2a308d5e410911a0
Ahmad Yoosofan <yoosofan@gmx.com>
2021-02-27 10:47:38 +0330

    fotohi

---
+++ exercise/13991209/db2.c
@@ -0,0 +1,301 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+typedef struct address{
+    char city[50];
+    char street[50];
+    char alley[50];
+    char complex[50];
+    int floor;
+}Address;
+typedef struct producer{
+    char city[50];
+    char street[50];
+    char alley[50];
+    char complex[50];
+    int floor;
+}producer;
+
+*/
+typedef struct customer{
+    int code;
+    char bday[10];
+    int phonenum;
+    char first_name[50];
+    char last_name[50];
+}Customer;
+
+typedef struct goods{
+    int code;
+    int number;
+    float price;
+    char name[1000];
+    char extra_info[1000];
+
+}Good;
+
+typedef struct sell{
+    int code;
+    int customer_code;
+    int purchase_code[100];
+    int purchase_number;
+    float final_price;
+    char date[10];
+}Sell;
+
+int main(){
+
+    //X
+    char l;
+    int req=0;
+    int t=1;
+
+    int customer_number=0;
+    int goods_number=0;
+    int sell_number=0;
+
+    Good good[1000];
+    Customer customer[100];
+    Sell sell[10];
+
+    while(t){
+        system("cls");
+        printf("1 - Add goods\n");
+        printf("2 - Add a customer\n");
+        printf("3 - Sales\n");
+        printf("4 - List of goods\n");
+        printf("5 - List of customers\n");
+        printf("6 - Sales list\n");
+        printf("7 - Factor information\n");
+        printf("8 - Sales information for a specific day\n");
+        printf("9 - Exit\n");
+
+
+        scanf("%d",&req);
+
+        switch (req){
+
+            case 1:
+                system("cls");
+                printf("Add a new good:\n");
+
+                goods_number++;
+
+                printf("Code:");
+                good[goods_number].code=goods_number;
+                printf("%d\n",goods_number);
+                printf("Name:");
+                scanf("%s",&good[goods_number].name);
+                printf("Price:");
+                scanf("%f",&good[goods_number].price);
+                printf("Existence:");
+                scanf("%d",&good[goods_number].number);
+                printf("Extra Information:");
+                scanf("%s",&good[goods_number].extra_info);
+
+                printf("DONE!");
+
+                printf("Add a char to exit ");
+                scanf("%s",&l);
+
+                break;
+
+            case 2:
+                system("cls");
+
+                customer_number++;
+                printf("Code:");
+                customer[customer_number].code=customer_number;
+                printf("First Name:");
+                scanf("%s",&customer[customer_number].first_name);
+                printf("Last Name:");
+                scanf("%s",&customer[customer_number].last_name);
+                printf("Phone number:");
+                scanf("%d",&customer[customer_number].phonenum);
+                printf("Birthday:");
+                scanf("%s",&customer[customer_number].bday);
+
+                //Address
+                printf("DONE!");
+
+                printf("Add a char to exit ");
+                scanf("%s",&l);
+                break;
+
+            case 3:
+                system("cls");
+                sell_number++;
+                int m=0;
+                int p=0;
+                printf("Sell Code:");
+                sell[sell_number].code=sell_number;
+                printf("Date:");
+                scanf("%s",&sell[sell_number].date);
+                printf("Customer code:");
+                scanf("%d",&sell[sell_number].customer_code);
+                sell[sell_number].final_price=0;
+                sell[sell_number].purchase_number=0;
+                while(1){
+                    printf("Enter the product code / or -1 If the purchase is over:");
+                    scanf("%d",&m);
+                    if (m == -1) break;
+                    else {
+                        if(m<=goods_number){
+                            if(good[m].number>0){
+                                p++;
+                                good[m].number--;
+                                sell[sell_number].purchase_number++;
+                                sell[sell_number].final_price+=good[m].price;
+                                sell[sell_number].purchase_code[p]=m;
+
+                            }
+                            else printf("this product dose not exist anymore");
+                            for (int i=1;i<=goods_number;i++){
+                                if (good[i].number==0){
+                                    printf("code number%d out of stock",good[i].code);
+                                    goods_number--;
+                                    for(int j=i;j<=goods_number;j++){
+                                        good[i].code=good[i+1].code;
+                                        *good[i].extra_info=&good[i+1].extra_info;
+                                        *good[i].name=&good[i+1].name;
+                                        good[i].number=good[i+1].number;
+                                        good[i].price=good[i+1].price;
+                                }
+
+                                }
+                            }
+                        }
+                        else printf("this product dose not exist");
+
+                        }
+
+
+                    }
+
+
+                //Address
+                printf("DONE!");
+
+                printf("Add a char to exit ");
+                scanf("%s",&l);
+
+                break;
+
+            case 4:
+                system("cls");
+                printf("List of goods\n");
+                for(int i=1;i<=goods_number;i++){
+                    printf("%d - Code :%d\n",i,good[i].code);
+                    printf("    Name :%s\n",good[i].name);
+                    printf("    Extra Information :%s\n",good[i].extra_info);
+                    printf("    Price :%f\n",good[i].price);
+                    printf("    Existance :%d\n\n",good[i].number);
+                }
+                printf("Add a char to exit ");
+                scanf("%s",&l);
+
+                break;
+
+            case 5:
+                system("cls");
+
+                printf("List of customers\n");
+                for(int i=1;i<=customer_number;i++){
+                    printf("%d - First Name :%s\n",i,customer[i].first_name);
+                    printf("    Last Name :%s\n",customer[i].last_name);
+                    printf("    Birthday :%s\n",customer[i].bday);
+                    printf("    Phone number :%d\n",customer[i].phonenum);
+                   // Address
+                }
+
+                printf("Add a char to exit ");
+                scanf("%s",&l);
+
+                break;
+
+            case 6:
+                system("cls");
+
+                printf("List of Sells\n");
+                for(int i=1;i<=sell_number;i++){
+                    printf("%d - Code :%d\n",i,sell[i].code);
+                    printf("    customer code :%s\n",sell[i].customer_code);
+                    printf("    date :%s\n",sell[i].date);
+                    printf("    final price :%f\n",sell[i].final_price);
+                    printf("Products:");
+                    for(int j=1;j<=sell[i].purchase_number;j++){
+                        printf("    product code :%d\n",sell[i].purchase_code[j]);
+
+                    }
+                }
+
+                printf("Add a char to exit ");
+                scanf("%s",&l);
+
+                break;
+
+            case 7:
+                system("cls");
+
+                int n=0;
+                printf("Which factor information do you want to see? Enter its code");
+                scanf("%d",&n);
+                if(n<=sell_number){
+                    printf("Code :%d\n",sell[n].code);
+                    printf("Date :%s\n",sell[n].date);
+                    printf("Customer Code :%d\n",sell[n].customer_code);
+                    printf("Customer Name :%s %s\n",customer[sell[n].customer_code].first_name,customer[sell[n].customer_code].last_name);
+
+                    for(int i=0;i<sell[n].purchase_number;i++){
+                        printf("Product with%dCode Name of Product :%s",sell[n].purchase_code[i],good[sell[n].purchase_code[i]].name);
+                    }
+                }
+
+                printf("Add a char to exit ");
+                scanf("%s",&l);
+
+                break;
+
+            case 8:
+                system("cls");
+                char s[10];
+                printf("Enter date");
+                scanf("%s",&s);
+                int e=1;
+                for(int i =1;i<=sell_number;i++)
+                {
+                    for(int j =0 ;j<10;j++)
+                        if (s[j] != sell[i].date[j])e=0;
+                    if (e){
+                    printf("Code :%d\n",sell[n].code);
+                    printf("Date :%s\n",sell[n].date);
+                    printf("Customer Code :%d\n",sell[n].customer_code);
+                    printf("Customer Name :%s %s\n",customer[sell[n].customer_code].first_name,customer[sell[n].customer_code].last_name);
+
+                    for(int i=0;i<sell[n].purchase_number;i++){
+                        printf("Product with%dCode Name of Product :%s",sell[n].purchase_code[i],good[sell[n].purchase_code[i]].name);
+                    }
+
+
+                    }
+                }
+                printf("Add a char to exit ");
+                scanf("%s",&l);
+                break;
+            case 9:
+                t=0;
+                system("cls");
+                printf("DONE !");
+                break;
+            default:
+                printf("your request is not valid\n");
+                printf("add a char and Try something else ");
+                scanf("%s",&l);
+                break;
+
+            }
+
+    }
+    return 0;
+
+}
--- exercise/13991209/t2.cpp
+++ exercise/13991209/t2.cpp
@@ -10,7 +10,6 @@ struct product
   int price_one;
   int whole_sale;
   int available_number;
-  char producer_compony;
   int produce_date;
   int remaining;
 
@@ -66,7 +65,7 @@ void Add_goods ()
   scanf ("%d", &p_t[counter_p].available_number);
 
   printf ("enter Compony_name  \n");
-  scanf ("%s", p_t[counter_p].producer_compony);
+  scanf ("%c", &p_t[counter_p].producer_compony);
 
   printf ("enter Date of produce\n");
   scanf ("%d", &p_t[counter_p].produce_date);
@@ -267,14 +266,7 @@ main ()
 
 }
 /*
-13991209$ g++ t2.cpp 
-t2.cpp: In function ‘void Add_goods()’:
-t2.cpp:51:12: warning: format ‘%s’ expects argument of type ‘char*’, but argument 2 has type ‘char (*)[50]’ [-Wformat=]
-   51 |   scanf ("%s", &p_t[counter_p].product_name);
-      |           ~^   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-      |            |   |
-      |            |   char (*)[50]
-      |            char*
+2.cpp: In function ‘void Add_goods()’:
 t2.cpp:60:12: warning: format ‘%f’ expects argument of type ‘float*’, but argument 2 has type ‘int*’ [-Wformat=]
    60 |   scanf ("%f", &p_t[counter_p].price_one);
       |           ~^   ~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -289,28 +281,15 @@ t2.cpp:63:12: warning: format ‘%f’ expects argument of type ‘float*’, bu
       |            |   int*
       |            float*
       |           %d
+t2.cpp:69:12: warning: format ‘%s’ expects argument of type ‘char*’, but argument 2 has type ‘int’ [-Wformat=]
+   69 |   scanf ("%s", p_t[counter_p].producer_compony);
+      |           ~^   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      |            |                  |
+      |            char*              int
 t2.cpp:77:3: error: ‘menu’ was not declared in this scope
    77 |   menu ();
       |   ^~~~
 t2.cpp: In function ‘void Add_customer()’:
-t2.cpp:84:12: warning: format ‘%s’ expects argument of type ‘char*’, but argument 2 has type ‘char (*)[50]’ [-Wformat=]
-   84 |   scanf ("%s", &c_t[counter_c].name);
-      |           ~^   ~~~~~~~~~~~~~~~~~~~~
-      |            |   |
-      |            |   char (*)[50]
-      |            char*
-t2.cpp:92:12: warning: format ‘%s’ expects argument of type ‘char*’, but argument 2 has type ‘char (*)[200]’ [-Wformat=]
-   92 |   scanf ("%s", &c_t[counter_c].address);
-      |           ~^   ~~~~~~~~~~~~~~~~~~~~~~~
-      |            |   |
-      |            |   char (*)[200]
-      |            char*
-t2.cpp:96:12: warning: format ‘%s’ expects argument of type ‘char*’, but argument 2 has type ‘char (*)[20]’ [-Wformat=]
-   96 |   scanf ("%s", &c_t[counter_c].phone_number);
-      |           ~^   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-      |            |   |
-      |            |   char (*)[20]
-      |            char*
 t2.cpp:99:3: error: ‘menu’ was not declared in this scope
    99 |   menu ();
       |   ^~~~
@@ -326,6 +305,54 @@ t2.cpp:119:3: error: ‘menu’ was not declared in this scope
   119 |   menu();
       |   ^~~~
 t2.cpp: In function ‘void show_list_goods()’:
+t2.cpp:127:17: warning: format ‘%d’ expects argument of type ‘int’, but argument 2 has type ‘int*’ [-Wformat=]
+  127 |       printf ("%d", &p_t[i].code_of_product);
+      |                ~^   ~~~~~~~~~~~~~~~~~~~~~~~
+      |                 |   |
+      |                 int int*
+      |                %n
+t2.cpp:129:17: warning: format ‘%d’ expects argument of type ‘int’, but argument 2 has type ‘int*’ [-Wformat=]
+  129 |       printf ("%d", &p_t[i].popularity);
+      |                ~^   ~~~~~~~~~~~~~~~~~~
+      |                 |   |
+      |                 int int*
+      |                %n
+t2.cpp:130:17: warning: format ‘%d’ expects argument of type ‘int’, but argument 2 has type ‘int*’ [-Wformat=]
+  130 |       printf ("%d", &p_t[i].ext);
+      |                ~^   ~~~~~~~~~~~
+      |                 |   |
+      |                 int int*
+      |                %n
+t2.cpp:131:17: warning: format ‘%d’ expects argument of type ‘int’, but argument 2 has type ‘int*’ [-Wformat=]
+  131 |       printf ("%d", &p_t[i].produce_date);
+      |                ~^   ~~~~~~~~~~~~~~~~~~~~
+      |                 |   |
+      |                 int int*
+      |                %n
+t2.cpp:132:17: warning: format ‘%d’ expects argument of type ‘int’, but argument 2 has type ‘int*’ [-Wformat=]
+  132 |       printf ("%d", &p_t[i].price_one);
+      |                ~^   ~~~~~~~~~~~~~~~~~
+      |                 |   |
+      |                 int int*
+      |                %n
+t2.cpp:133:17: warning: format ‘%d’ expects argument of type ‘int’, but argument 2 has type ‘int*’ [-Wformat=]
+  133 |       printf ("%d", &p_t[i].whole_sale);
+      |                ~^   ~~~~~~~~~~~~~~~~~~
+      |                 |   |
+      |                 int int*
+      |                %n
+t2.cpp:134:17: warning: format ‘%d’ expects argument of type ‘int’, but argument 2 has type ‘int*’ [-Wformat=]
+  134 |       printf ("%d", &p_t[i].remaining);
+      |                ~^   ~~~~~~~~~~~~~~~~~
+      |                 |   |
+      |                 int int*
+      |                %n
+t2.cpp:135:17: warning: format ‘%d’ expects argument of type ‘int’, but argument 2 has type ‘int*’ [-Wformat=]
+  135 |       printf ("%d", &p_t[i].available_number);
+      |                ~^   ~~~~~~~~~~~~~~~~~~~~~~~~
+      |                 |   |
+      |                 int int*
+      |                %n
 t2.cpp:136:22: error: invalid conversion from ‘char’ to ‘const char*’ [-fpermissive]
   136 |       printf (p_t[i].producer_compony);
       |               ~~~~~~~^~~~~~~~~~~~~~~~
@@ -353,6 +380,7 @@ t2.cpp:162:17: warning: format ‘%d’ expects argument of type ‘int’, but 
 t2.cpp:169:5: error: ‘menu’ was not declared in this scope
   169 |     menu();
       |     ^~~~
+t2.cpp: In function ‘void show_factor_of_id()’:
 t2.cpp:183:23: warning: format ‘%d’ expects argument of type ‘int’, but argument 2 has type ‘long int’ [-Wformat=]
   183 |             printf ("%d\n ",s_t[i].code_of_product);
       |                      ~^     ~~~~~~~~~~~~~~~~~~~~~~
@@ -371,5 +399,7 @@ t2.cpp:205:23: warning: format ‘%d’ expects argument of type ‘int’, but 
       |                      %ld
 t2.cpp:213:5: error: ‘menu’ was not declared in this scope
   213 |     menu();
-      |     
+      |     ^~~~
+13991209$ 
+
 */
